4cb07adf28db4d1518606db578c67a2f864d69c0
techhouse_150908 <kmaehara@mel.co.jp>
2023-05-10 16:51:23 +0900

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,43 @@
 #include <M5Core2.h>
 
+
 void setup() {
   M5.begin();
-  M5.Lcd.println("Press the left button");
-  Serial.println("test start");
+  
+  // ボタンA、ボタンB、ボタンCのイベントリスナーを設定
+  //M5.BtnA.setPressedHandler(button_a_was_pressed);
+  //M5.BtnB.setPressedHandler(button_b_was_pressed);
+  //M5.BtnC.setPressedHandler(button_c_was_pressed);
 }
 
 void loop() {
   M5.update();
+
   if (M5.BtnA.wasPressed()) {
-    M5.Lcd.fillScreen(TFT_BLACK);
+    M5.Lcd.clear();
+    M5.Lcd.fillScreen(TFT_WHITE);
+    M5.Lcd.setTextColor(TFT_GREEN);
+    M5.Lcd.setTextSize(3);
     M5.Lcd.setCursor(0, 0);
-    M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
-    M5.Lcd.setTextSize(2);
-    M5.Lcd.println("hello world");
-    Serial.println("pushed A");
+    M5.Lcd.print("hello world");
   }
+
+  if (M5.BtnB.wasPressed()) {
+    M5.Lcd.clear();
+    M5.Lcd.fillScreen(TFT_BLACK);
+    M5.Lcd.setTextColor(TFT_YELLOW);
+    M5.Lcd.setTextSize(3);
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.print("hello world");
+  }
+
+  if (M5.BtnC.wasPressed()) {
+    M5.Lcd.clear();
+    M5.Lcd.fillScreen(TFT_YELLOW);
+    M5.Lcd.setTextColor(TFT_RED);
+    M5.Lcd.setTextSize(3);
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.print("hello world");
+  }
+
 }
