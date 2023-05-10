db5ed45b794d68a09d5f73c81a449d1e420e69da
techhouse_150908 <kmaehara@mel.co.jp>
2023-05-10 14:49:21 +0900

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,24 @@
 #include <M5Core2.h>
 
+#define BUZZER_PIN 25 // ブザーに接続されたピン番号
+
 void setup() {
   M5.begin();
-  M5.Lcd.println("Press the left button");
   Serial.println("test start");
+  pinMode(BUZZER_PIN, OUTPUT);
 }
 
 void loop() {
   M5.update();
-  if (M5.BtnA.wasPressed()) {
-    M5.Lcd.fillScreen(TFT_BLACK);
-    M5.Lcd.setCursor(0, 0);
-    M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
-    M5.Lcd.setTextSize(2);
-    M5.Lcd.println("hello world");
-    Serial.println("pushed A");
+  if (M5.BtnA.wasReleased()) {
+    // ビープ音を鳴らす
+    //M5.Speaker.tone(1000, 500); // 周波数1000Hz、長さ500msのビープ音
+    tone(BUZZER_PIN, 1000, 500); // ピン番号、周波数1000Hz、長さ500msのビープ音
+    delay(500); // 500ms待機して音を停止
+    noTone(BUZZER_PIN); // ブザーを停止
+
+
+    //M5.Lcd.println("beepsound");
+    Serial.println("beepsound");
   }
 }
