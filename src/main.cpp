550c7ce268e2c1656018b182c8d166073bc52b94
techhouse_150908 <kmaehara@mel.co.jp>
2023-05-11 11:08:57 +0900

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,64 @@
 #include <M5Core2.h>
 #include <WiFi.h>
 
+const char* ssid;
+const char* password;
+
+void connectToWiFi();
+
+
 void setup() {
   M5.begin();
   //M5.Power.begin();
-  M5.Lcd.printf("Wi-Fi Configuration\n");
-  delay(1000);
-  
-  // Wi-Fi設定モードを開始
-  WiFi.beginSmartConfig();
-  
-  // Wi-Fi接続待機
-  while (!WiFi.smartConfigDone()) {
-    delay(500);
-    M5.Lcd.print(".");
+  M5.Lcd.clear();
+
+  // SRAM領域からSSIDとパスワードを読み込む
+  ssid = (const char*)RTC_SLOW_MEM;
+  password = (const char*)RTC_SLOW_MEM + strlen(ssid) + 1;
+
+  // SRAM領域に保存されたSSIDとパスワードが空でない場合、Wi-Fiに接続する
+  if (strlen(ssid) > 0 && strlen(password) > 0) {
+    connectToWiFi();
+  } else {
+    // Wi-Fi設定モードを開始
+    WiFi.beginSmartConfig();
+
+    // Wi-Fi接続待機
+    while (!WiFi.smartConfigDone()) {
+      delay(500);
+      M5.Lcd.print(".");
+    }
+
+    // Wi-Fiに接続したらSSIDとパスワードを保存
+    strcpy((char*)RTC_SLOW_MEM, WiFi.SSID().c_str());
+    strcpy((char*)RTC_SLOW_MEM + strlen(ssid) + 1, WiFi.psk().c_str());
   }
-  
-  // Wi-Fi接続が完了したら、SSIDとパスワードを表示
-  M5.Lcd.printf("\n\nWi-Fi Configuration Completed!\n");
-  M5.Lcd.printf("SSID: %s\n", WiFi.SSID().c_str());
-  M5.Lcd.printf("Password: %s\n", WiFi.psk().c_str());
-  
-  // 接続されたWi-Fiネットワークの詳細を表示
-  M5.Lcd.printf("IP Address: %s\n", WiFi.localIP().toString().c_str());
-  M5.Lcd.printf("Gateway: %s\n", WiFi.gatewayIP().toString().c_str());
-  M5.Lcd.printf("Subnet Mask: %s\n", WiFi.subnetMask().toString().c_str());
 }
 
 void loop() {
-  // 何もしない
+  // メインの処理をここに記述
+
+  // Wi-Fiが切断された場合、再接続を試みる
+  if (WiFi.status() != WL_CONNECTED) {
+    connectToWiFi();
+  }
+  
+  // その他の処理をここに記述
+}
+
+void connectToWiFi() {
+  M5.Lcd.clear();
+  M5.Lcd.println("Connecting to Wi-Fi...");
+
+  // 保存されたSSIDとパスワードを使用してWi-Fiに接続
+  WiFi.begin(ssid, password);
+  
+  // Wi-Fi接続待機
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    M5.Lcd.print(".");
+  }
+
+  M5.Lcd.clear();
+  M5.Lcd.println("Wi-Fi Connected");
 }
