af25b2099a6907bcb69273447cd1bb1f7c516a2c
techhouse_150908 <kmaehara@mel.co.jp>
2023-05-11 11:39:36 +0900

    test build ok

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,101 @@
 #include <M5Core2.h>
 #include <WiFi.h>
+#include <SPIFFS.h>
+#include <ArduinoJson.h>
 
-const char* ssid;
-const char* password;
+const char *configFile = "/wifi_config.json";
+const char *ssid;
+const char *password;
 
+void loadWifiConfig();
+void saveWifiConfig(const char *ssid, const char *password);
 void connectToWiFi();
 
 
-void setup() {
+void setup()
+{
   M5.begin();
-  //M5.Power.begin();
-  M5.Lcd.clear();
+  Serial.begin(115200);
 
-  // SRAM領域からSSIDとパスワードを読み込む
-  ssid = (const char*)RTC_SLOW_MEM;
-  password = (const char*)RTC_SLOW_MEM + strlen(ssid) + 1;
-
-  // SRAM領域に保存されたSSIDとパスワードが空でない場合、Wi-Fiに接続する
-  if (strlen(ssid) > 0 && strlen(password) > 0) {
-    connectToWiFi();
-  } else {
-    // Wi-Fi設定モードを開始
-    WiFi.beginSmartConfig();
-
-    // Wi-Fi接続待機
-    while (!WiFi.smartConfigDone()) {
-      delay(500);
-      M5.Lcd.print(".");
-    }
-
-    // Wi-Fiに接続したらSSIDとパスワードを保存
-    strcpy((char*)RTC_SLOW_MEM, WiFi.SSID().c_str());
-    strcpy((char*)RTC_SLOW_MEM + strlen(ssid) + 1, WiFi.psk().c_str());
+  if (!SPIFFS.begin(true))
+  {
+    Serial.println("An error occurred while mounting SPIFFS");
+    return;
   }
+
+  loadWifiConfig();
+  connectToWiFi();
 }
 
-void loop() {
-  // メインの処理をここに記述
+void loop()
+{
+  // WiFi接続後の処理をここに記述
 
-  // Wi-Fiが切断された場合、再接続を試みる
-  if (WiFi.status() != WL_CONNECTED) {
-    connectToWiFi();
-  }
-  
-  // その他の処理をここに記述
 }
 
-void connectToWiFi() {
-  M5.Lcd.clear();
-  M5.Lcd.println("Connecting to Wi-Fi...");
+void loadWifiConfig()
+{
+  if (!SPIFFS.exists(configFile))
+  {
+    Serial.println("WiFi config file not found");
+    return;
+  }
 
-  // 保存されたSSIDとパスワードを使用してWi-Fiに接続
+  File configFile = SPIFFS.open("/config.txt", "r");
+  if (!configFile)
+  {
+    Serial.println("Failed to open WiFi config file");
+    return;
+  }
+
+  // WiFi設定情報の読み込み
+  StaticJsonDocument<256> jsonConfig;
+  DeserializationError error = deserializeJson(jsonConfig, configFile);
+  if (error)
+  {
+    Serial.println("Failed to parse WiFi config file");
+    return;
+  }
+
+  ssid = jsonConfig["ssid"];
+  password = jsonConfig["password"];
+
+  configFile.close();
+}
+
+void saveWifiConfig(const char *ssid, const char *password)
+{
+  StaticJsonDocument<256> jsonConfig;
+  jsonConfig["ssid"] = ssid;
+  jsonConfig["password"] = password;
+
+  File configFile = SPIFFS.open("/config.txt", "w");
+  if (!configFile)
+  {
+    Serial.println("Failed to open WiFi config file for writing");
+    return;
+  }
+
+  serializeJson(jsonConfig, configFile);
+  configFile.close();
+}
+
+void connectToWiFi()
+{
+  if (!ssid || !password)
+  {
+    Serial.println("No WiFi config available");
+    return;
+  }
+
   WiFi.begin(ssid, password);
-  
-  // Wi-Fi接続待機
-  while (WiFi.status() != WL_CONNECTED) {
+  Serial.print("Connecting to WiFi");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
-    M5.Lcd.print(".");
+    Serial.print(".");
   }
 
-  M5.Lcd.clear();
-  M5.Lcd.println("Wi-Fi Connected");
+  Serial.println("\nConnected to WiFi");
 }
