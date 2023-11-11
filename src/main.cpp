1b2ed36b791bea8ad1f1f578d98ddabad8df7d6a
KenjiMaehara <kmaehara@mel.co.jp>
2023-11-11 09:31:31 +0900

    現在地の表示機能を追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <M5Core2.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <ArduinoJson.h>
 
 // WiFi設定
 const char* ssid = "googlemain";
@@ -9,9 +10,10 @@ const char* password = "Fdsa@0813";
 // NTPサーバー
 const char* ntpServer = "pool.ntp.org";
 const long  gmtOffset_sec = 3600 * 9;  // 日本のGMTオフセット（UTC+9）
-const int   daylightOffset_sec = 3600;  // 夏時間のオフセット
+const int   daylightOffset_sec = 0;    // 夏時間は日本では適用されない
 
 void displayTime();
+void displayLocation();
 
 void setup() {
   M5.begin();
@@ -30,7 +32,8 @@ void setup() {
 void loop() {
   M5.update();
   displayTime();
-  delay(1000);
+  displayLocation();
+  delay(10000);  // 10秒ごとに更新
 }
 
 void displayTime() {
@@ -44,12 +47,36 @@ void displayTime() {
   sprintf(timeString, "%02d:%02d:%02d", timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
 
   int textWidth = M5.Lcd.textWidth(timeString);
-  int textHeight = 30;  // テキストの高さを大きくする
+  int textHeight = 30;
 
   int x = (320 - textWidth) / 2;
-  int y = (240 - textHeight) / 2;
+  int y = (240 - textHeight) / 2 - 20;
 
   M5.Lcd.fillScreen(TFT_BLACK);
   M5.Lcd.setCursor(x, y);
   M5.Lcd.println(timeString);
+}
+
+void displayLocation() {
+  HTTPClient http;
+  http.begin("http://ip-api.com/json/");
+  int httpCode = http.GET();
+
+  if (httpCode > 0) {
+    String payload = http.getString();
+    DynamicJsonDocument doc(1024);
+    deserializeJson(doc, payload);
+    String city = doc["city"].as<String>();
+    String country = doc["country"].as<String>();
+
+    M5.Lcd.setTextSize(2);
+    M5.Lcd.setCursor(10, 200);
+    M5.Lcd.println("Location: " + city + ", " + country);
+  } else {
+    M5.Lcd.setTextSize(2);
+    M5.Lcd.setCursor(10, 200);
+    M5.Lcd.println("Location: Unable to retrieve");
+  }
+
+  http.end();
 }
