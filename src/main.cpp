51b82a4313dfe90e87f40dbfdd5e3edea849dee2
KenjiMaehara <kmaehara@mel.co.jp>
2023-11-12 18:12:16 +0900

    テスト

--- src/main.cpp
+++ src/main.cpp
@@ -1,103 +1,40 @@
 #include <M5Core2.h>
-#include <WiFi.h>
-#include <HTTPClient.h>
-#include <ArduinoJson.h>
-#include "CUF_24px.h"  // フォントファイルをインクルード
-
-// WiFi設定
-const char* ssid = "googlemain";
-const char* password = "Fdsa@0813";
-
-// NTPサーバー
-const char* ntpServer = "pool.ntp.org";
-const long  gmtOffset_sec = 3600 * 9;  // 日本のGMTオフセット（UTC+9）
-const int   daylightOffset_sec = 0;    // 夏時間は日本では適用されない
-
-// 現在地情報を格納するグローバル変数
-String locationInfo = "Location: Unknown";
-
-void displayTime();
-void displayLocation();  // 位置情報を取得する関数
+#include <FS.h>
+#include <SD.h>
 
 void setup() {
+  // M5Stack Core2の初期化
   M5.begin();
-  WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    M5.Lcd.println("Connecting to WiFi...");
-  }
-
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  M5.Lcd.setTextColor(TFT_WHITE);
-  M5.Lcd.setTextSize(6);  // フォントサイズを大きくする
-
-  displayLocation();  // 電源投入時に一度だけ位置情報を取得
-
-  //M5.Lcd.setFreeFont(&unicode_24px);  // 日本語表示用のフォントを設定
-}
-
-void loop() {
-  M5.update();
-  displayTime();
-  delay(1000);  // 1秒ごとに更新
-}
-
-void displayTime() {
-  struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)) {
-    M5.Lcd.println("Failed to obtain time");
+  // SDカードの初期化
+  if (!SD.begin()) {
+    Serial.println("SD Card Mount Failed");
     return;
   }
 
-  char timeString[10];
-  sprintf(timeString, "%02d:%02d:%02d", timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
-
-  int textWidth = M5.Lcd.textWidth(timeString);
-  int textHeight = 30;
-
-  int x = 0;
-  int y = (240 - textHeight) / 2 - 20;
-  
-  M5.Lcd.setTextSize(1);
-  M5.Lcd.setTextFont(1);
-  M5.Lcd.setTextSize(6);
-
-  M5.Lcd.fillScreen(TFT_BLACK);
-  M5.Lcd.setCursor(x, y);
-  M5.Lcd.println(timeString);
-
-  // 現在地情報を表示
-  // 日本語の表示部分でフォントを使用
-  M5.Lcd.setFreeFont(&unicode_24px);
-  M5.Lcd.setTextSize(1);
-  M5.Lcd.setCursor(10, 200);
-  M5.Lcd.drawString(locationInfo, 10, 200);
+  // ボタンの配置
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setTextColor(WHITE, BLACK);
+  M5.Lcd.drawCentreString("Button 1", 80, 100, 4);
+  M5.Lcd.drawCentreString("Button 2", 160, 100, 4);
+  M5.Lcd.drawCentreString("Button 3", 240, 100, 4);
 }
 
-void displayLocation() {
-  HTTPClient http;
-  http.begin("http://ip-api.com/json/");
-  int httpCode = http.GET();
+void playWavFile(const char *filename) {
+  // WAVファイルの再生ロジックをここに記述
+  // ...
+}
 
-  if (httpCode > 0) {
-    String payload = http.getString();
-    DynamicJsonDocument doc(1024);
-    deserializeJson(doc, payload);
-    String city = doc["city"].as<String>();
+void loop() {
+  // M5Stack Core2の更新
+  M5.update();
 
-
-    // 簡単なマッピングテーブル
-    if (city == "Tokyo") {
-      locationInfo = "東京";
-    } else if (city == "Saitama") {
-      locationInfo = "埼玉";
-    } else {
-      locationInfo = city;  // その他の場合はそのまま表示
-    }
-  } else {
-    locationInfo = "Location: Unable to retrieve";
+  // 各ボタンのチェック
+  if (M5.BtnA.wasPressed()) {
+    playWavFile("/sound1.wav");
+  } else if (M5.BtnB.wasPressed()) {
+    playWavFile("/sound2.wav");
+  } else if (M5.BtnC.wasPressed()) {
+    playWavFile("/sound3.wav");
   }
-
-  http.end();
 }
