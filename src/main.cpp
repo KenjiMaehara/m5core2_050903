ee08fd0f04ecba59c002b2a27ef61718c7368693
techhouse_150908 <kmaehara@mel.co.jp>
2023-11-13 17:26:53 +0900

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,103 +1,54 @@
 #include <M5Core2.h>
 #include <WiFi.h>
-#include <HTTPClient.h>
-#include <ArduinoJson.h>
-#include "CUF_24px.h"  // フォントファイルをインクルード
+#include <time.h>
 
-// WiFi設定
+// WiFi設定 - ここにあなたのWiFiのSSIDとパスワードを入力してください
 const char* ssid = "20230616me_IPv6";
 const char* password = "asdf0616";
 
-// NTPサーバー
+// NTPサーバーの設定
 const char* ntpServer = "pool.ntp.org";
-const long  gmtOffset_sec = 3600 * 9;  // 日本のGMTオフセット（UTC+9）
-const int   daylightOffset_sec = 0;    // 夏時間は日本では適用されない
-
-// 現在地情報を格納するグローバル変数
-String locationInfo = "Location: Unknown";
-
-void displayTime();
-void displayLocation();  // 位置情報を取得する関数
+const long  gmtOffset_sec = 3600 * 9;  // GMT+9:00 for Japan
+const int   daylightOffset_sec = 0;
 
 void setup() {
+  // M5Stack Core2の初期化
   M5.begin();
-  WiFi.begin(ssid, password);
 
+  // WiFi接続
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    M5.Lcd.println("Connecting to WiFi...");
+    M5.Lcd.print(".");
   }
-
+  
+  // NTPサーバーから時刻を取得
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  M5.Lcd.setTextColor(TFT_WHITE);
-  M5.Lcd.setTextSize(6);  // フォントサイズを大きくする
 
-  displayLocation();  // 電源投入時に一度だけ位置情報を取得
-
-  //M5.Lcd.setFreeFont(&unicode_24px);  // 日本語表示用のフォントを設定
+  // LCDの設定
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.setTextColor(WHITE);
+  M5.Lcd.fillScreen(BLACK);
 }
 
 void loop() {
-  M5.update();
-  displayTime();
-  delay(1000);  // 1秒ごとに更新
-}
-
-void displayTime() {
+  // 現在の時刻を取得
+  time_t now;
   struct tm timeinfo;
   if (!getLocalTime(&timeinfo)) {
-    M5.Lcd.println("Failed to obtain time");
+    M5.Lcd.print("Failed to obtain time");
     return;
   }
+  now = mktime(&timeinfo);
 
-  char timeString[10];
-  sprintf(timeString, "%02d:%02d:%02d", timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
+  // 日付と曜日をフォーマット
+  char strftime_buf[64];
+  strftime(strftime_buf, sizeof(strftime_buf), "%m/%d (%a)", &timeinfo);
 
-  int textWidth = M5.Lcd.textWidth(timeString);
-  int textHeight = 30;
+  // LCDに日付を表示
+  M5.Lcd.setCursor(10, 10);
+  M5.Lcd.print(strftime_buf);
 
-  int x = 0;
-  int y = (240 - textHeight) / 2 - 20;
-  
-  M5.Lcd.setTextSize(1);
-  M5.Lcd.setTextFont(1);
-  M5.Lcd.setTextSize(6);
-
-  M5.Lcd.fillScreen(TFT_BLACK);
-  M5.Lcd.setCursor(x, y);
-  M5.Lcd.println(timeString);
-
-  // 現在地情報を表示
-  // 日本語の表示部分でフォントを使用
-  M5.Lcd.setFreeFont(&unicode_24px);
-  M5.Lcd.setTextSize(1);
-  M5.Lcd.setCursor(10, 200);
-  M5.Lcd.drawString(locationInfo, 10, 200);
-}
-
-void displayLocation() {
-  HTTPClient http;
-  http.begin("http://ip-api.com/json/");
-  int httpCode = http.GET();
-
-  if (httpCode > 0) {
-    String payload = http.getString();
-    DynamicJsonDocument doc(1024);
-    deserializeJson(doc, payload);
-    String city = doc["city"].as<String>();
-
-
-    // 簡単なマッピングテーブル
-    if (city == "Tokyo") {
-      locationInfo = "東京都";
-    } else if (city == "Chiyoda") {
-      locationInfo = "千代田区";
-    } else {
-      locationInfo = city;  // その他の場合はそのまま表示
-    }
-  } else {
-    locationInfo = "Location: Unable to retrieve";
-  }
-
-  http.end();
+  // 1秒待機
+  delay(1000);
 }
