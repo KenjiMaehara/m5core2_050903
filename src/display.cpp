9c96dfe0e69e28f864f8c64d0581e0ec69c4a30b
techhouse_150908 <kmaehara@mel.co.jp>
2023-11-16 14:16:16 +0900

    ファイル追加

---
+++ src/display.cpp
@@ -0,0 +1,16 @@
+
+void displayTime(int duration) {
+  // 時刻を表示するコード
+}
+
+void displayWeather(int duration) {
+  // 天気情報を取得して表示するコード
+}
+
+void displayLocationWeather(int duration) {
+  // 現在地の天気を表示するコード
+}
+
+void displayTemperatureAndHumidity(int duration) {
+  // 温度と湿度を表示するコード
+}
