5297c7c4ebf28ee6bf168ae207815deb8ba23511
KenjiMaehara <kmaehara@mel.co.jp>
2024-02-17 15:26:12 +0900

    一旦コミット。

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,12 @@ void setup() {
   M5.begin();
   M5.Lcd.println("Hello World");
   M5.Spk.begin();  // Initialize the speaker.
-  M5.Spk.DingDong();  // Play the DingDong sound.
-  delay(100);
+
+  // PWMチャンネル0を初期化、GPIO25に設定（M5Stack Core2の内蔵スピーカー）
+  ledcSetup(0, 2000, 8);
+  ledcAttachPin(25, 0);
+
+  M5.Lcd.println("Setup Complete");
 }
 
 
@@ -22,6 +26,11 @@ void loop() {
   } else if (M5.BtnB.wasPressed()) {
     M5.Lcd.println("Button B");
     //M5.Spk.tone(2000, 100);  // Play a 2000Hz tone for 100ms.
+    
+    ledcWriteTone(0, 2000);
+    delay(1000);  // 100ミリ秒待つ
+    ledcWriteTone(0, 0);  // トーンを停止
+
   } else if (M5.BtnC.wasPressed()) {
     M5.Lcd.println("Button C");
     //M5.Spk.tone(3000, 100);  // Play a 3000Hz tone for 100ms.
