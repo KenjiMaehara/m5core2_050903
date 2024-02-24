3b07bf176e802ad151f1eb30a1ad98224746afdc
KenjiMaehara <kmaehara@mel.co.jp>
2024-02-24 09:28:18 +0900

    さらに修正

--- src/payload.h
+++ src/payload.h
@@ -5,6 +5,17 @@
 
 // ペイロード作成関数のプロトタイプ宣言
 //String createJsonPayload(String deviceName, String deviceId, String organization, unsigned long timestamp, String sensor, String button, String routineCheck, String spare1, String spare2);
-String createJsonPayload(const String& deviceName, const String& deviceId, const String& organization, unsigned long timestamp, const String& sensor, const String& button, const String& routineCheck, const String& spare1, const String& spare2);
+String createJsonPayload(
+    const String& deviceName, 
+    const String& deviceId, 
+    const String& organization, 
+    unsigned long timestamp, 
+    const String& sensor, 
+    const String& button, 
+    const String& 
+    routineCheck, 
+    const String& spare1, 
+    const String& spare2
+    );
 
 #endif // PAYLOAD_H
