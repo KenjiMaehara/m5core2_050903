5745e66b5a78a1bfef11157d29b51f79d2fcb0df
techhouse_150908 <kmaehara@mel.co.jp>
2024-02-27 11:46:46 +0900

    現在もまだうまくいかない。

--- src/audio.cpp
+++ src/audio.cpp
@@ -12,18 +12,30 @@ AudioGeneratorMP3 *mp3;
 AudioFileSourceSD *file;
 AudioOutputI2S *out;
 
+#define OUTPUT_GAIN 10
+
 void audio_setup() {
   M5.begin();
+
+  M5.Axp.SetSpkEnable(true);
+
+  out = new AudioOutputI2S(0, 1); // I2Sデータピンとクロックピンを指定
+  out->SetPinout(12, 0, 2);
+  //out->SetGain(0.3); // 音量を設定
+  out->SetOutputModeMono(true);
+  out->SetGain((float)OUTPUT_GAIN/100.0);
+  file = new AudioFileSourceSD("/test.mp3"); // SDカードのルートにあるMP3ファイル
+  mp3 = new AudioGeneratorMP3();
+
   // Create a task that will execute the PlayMP3Task() function, with a name of "PlayMP3"
   xTaskCreatePinnedToCore(PlayMP3Task, "PlayMP3", 10000, NULL, 1, NULL, 0);
 }
 
 
 void PlayMP3Task(void *parameter) {
-  out = new AudioOutputI2S(0, 1); // I2Sデータピンとクロックピンを指定
-  out->SetGain(0.125); // 音量を設定
-  file = new AudioFileSourceSD("/test.mp3"); // SDカードのルートにあるMP3ファイル
-  mp3 = new AudioGeneratorMP3();
+
+
+
   if (mp3->begin(file, out)) {
     while (mp3->isRunning()) {
       mp3->loop();
