a221609d902a693788ccc2acdf9ddb9448d4eeba
techhouse_150908 <kmaehara@mel.co.jp>
2024-02-27 13:11:19 +0900

    さらに修正

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <M5Core2.h>
-#include "audio.h"
 #include <AudioFileSourceSD.h>
 #include <AudioGeneratorMP3.h>
 #include <AudioOutputI2S.h>
@@ -8,18 +7,36 @@
 const char* ssid = "YOUR_WIFI_SSID";
 const char* password = "YOUR_WIFI_PASSWORD";
 
+AudioGeneratorMP3 *mp3;
+AudioFileSourceSD *file;
+AudioOutputI2S *out;
 
+void audio_setup();
+
+#define OUTPUT_GAIN 10
 
 void setup() {
 
-
   audio_setup();
-
-  //のwhile(1);
 
 }
 
 
+
+void audio_setup() {
+  M5.begin();
+
+  M5.Axp.SetSpkEnable(true);
+
+  out = new AudioOutputI2S(0, 1); // I2Sデータピンとクロックピンを指定
+  out->SetPinout(12, 0, 2);
+  //out->SetGain(0.3); // 音量を設定
+  out->SetOutputModeMono(true);
+  out->SetGain((float)OUTPUT_GAIN/100.0);
+  file = new AudioFileSourceSD("/test.mp3"); // SDカードのルートにあるMP3ファイル
+  mp3 = new AudioGeneratorMP3();
+
+}
 
 
 
