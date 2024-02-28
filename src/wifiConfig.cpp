dfb105d4989f9ebf9acd907add350c46be78e2a7
techhouse_150908 <kmaehara@mel.co.jp>
2024-02-28 11:17:51 +0900

    電源ON時に、IPアドレスを取得できたかどうか画面に表示するように修正。

--- src/wifiConfig.cpp
+++ src/wifiConfig.cpp
@@ -2,8 +2,21 @@
 #include <SPIFFS.h>      // ESP8266/ESP32 SPIFFSライブラリ
 #include <WiFi.h>        // ESP8266/ESP32 WiFiライブラリ
 #include "wifiConfig.h"  // このスケッチのヘッダファイル
+#include <M5Core2.h>
 
 void connectToWiFi() {
+
+  // M5Core2の初期化
+  M5.begin();
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setCursor(0, 0);
+  M5.Lcd.setTextColor(WHITE);
+  M5.Lcd.setTextSize(2);
+
+  // WiFi接続の設定...
+  Serial.print("Connecting to WiFi");
+  M5.Lcd.print("Connecting to WiFi");
+
   File wifiConfig = SPIFFS.open("/wifi_SSID.conf", "r");
   if (!wifiConfig) {
     Serial.println("設定ファイルのオープンに失敗しました。");
@@ -34,6 +47,7 @@ void connectToWiFi() {
     while (WiFi.status() != WL_CONNECTED && attempts < 30) {
       delay(500);
       Serial.print(".");
+      M5.Lcd.print(".");  // LCD画面に"."を表示
       attempts++;
     }
 
@@ -41,9 +55,19 @@ void connectToWiFi() {
       Serial.println("\nWiFiに接続しました。");
       Serial.print("IPアドレス: ");
       Serial.println(WiFi.localIP());
+
+      M5.Lcd.println("Connected to WiFi.");
+      M5.Lcd.print("IP Address: ");
+      M5.Lcd.println(WiFi.localIP());
+      
+      delay(5000); // 5秒間待機
+
+      M5.Lcd.fillScreen(BLACK); // 画面をクリアして表示を消す
+
       return;
     } else {
       Serial.println("\n接続に失敗しました。次のSSIDを試します。");
+      M5.Lcd.println("Failed to connect to WiFi.");
     }
   }
 
